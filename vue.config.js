a767715279a3b282dc2dad69d5eb864f7b981f90
qufan <allen5858@163.com>
2022-01-10 15:07:27 +0800

    first commit

---
+++ vue.config.js
@@ -0,0 +1,35 @@
+"use strict";
+const path = require("path");
+
+function resolve(dir) {
+  return path.join(__dirname, dir);
+}
+
+const port = process.env.port || process.env.npm_config_port || 8090;
+
+module.exports = {
+  publicPath: "/",
+  outputDir: "dist",
+  assetsDir: "static",
+  lintOnSave: process.env.NODE_ENV === "development",
+  productionSourceMap: false,
+  devServer: {
+    port: port,
+    open: true,
+    overlay: {
+      warnings: false,
+      errors: true,
+    },
+    before: require("./mock/mock-server.js"),
+  },
+  configureWebpack: {
+    // provide the app's title in webpack's name field, so that
+    // it can be accessed in index.html to inject the correct title.
+    name: name,
+    resolve: {
+      alias: {
+        '@': resolve('src')
+      }
+    }
+  },
+};
